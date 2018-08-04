50faed56ea0cb429fc97ea954858dc59b90d8544
Trish Whetzel <plwhetzel@gmail.com>
2018-08-04 23:38:27 +0100

    modify for pep8 formatting compliance

--- scripts/database/DBConnection.py
+++ scripts/database/DBConnection.py
@@ -1,18 +1,19 @@
 import cx_Oracle
 import gwas_data_sources
 
+
 class gwasCatalogDbConnector(object):
     '''
     General Oracle database connection methods.
     '''
-    
+
     def __init__(self, database_name):
 
         self.database_name = database_name
 
         # Trying to connect:
         try:
-            ip, port, sid, username, password = gwas_data_sources.get_db_properties(self.database_name)
+            ip, port, sid, username, password = gwas_data_sources.get_db_properties(self.database_name) # noqa
             dsn_tns = cx_Oracle.makedsn(ip, port, sid)
             self.connection = cx_Oracle.connect(username, password, dsn_tns)
             self.cursor = self.connection.cursor()
