f76e8b129af8660dd838953c617c1e18d462bf87
Trish Whetzel <plwhetzel@gmail.com>
2018-08-04 13:50:04 +0100

    repo reorganization

---
+++ scripts/ols/DataFormatter.py
@@ -0,0 +1,61 @@
+class DataFormatter:
+    """
+    Format results into JSON for further analysis.
+    """
+
+    def __init__(self, data):
+        self.data = data
+
+
+    def get_term_information(self):
+        term_result = self.data
+
+        results_list = []
+        result_obj = {}
+
+        keys = term_result.keys()
+        
+        label_key = "label"
+        iri_key = "iri"
+        synonyms_key = "synonyms"
+        short_form_key = "short_form"
+        definition_key = "description"
+        ancestors = "ancestors"
+        
+
+        if label_key in keys:
+            result_obj[label_key] = term_result[label_key]
+        if iri_key in keys:
+            result_obj[iri_key] = term_result[iri_key]
+        if synonyms_key in keys:
+            result_obj[synonyms_key] = term_result[synonyms_key]
+        if short_form_key in keys:
+            result_obj[short_form_key] = term_result[short_form_key]
+        if definition_key in keys:
+            result_obj[definition_key] = term_result[definition_key]
+
+        # Get link for ancestors
+        result_obj[ancestors] = term_result['_links']['ancestors']['href']
+
+        results_list.append(result_obj)
+
+        return result_obj
+
+
+    def get_ancestor_labels(self):
+        '''
+        Parse and return only term labels from the OLS "ancestors" link.
+        '''
+
+        ancestors = self.data
+
+        ancestor_labels = []
+
+        for term in ancestors['_embedded']['terms']:
+            # TODO: Do not include ontology metadata 
+            # terms, e.g. Thing, disposition, experimental factor ontology
+            ancestor_labels.append(term['label'])
+
+        return ancestor_labels
+
+
---
+++ scripts/ols/OLSData.py
@@ -0,0 +1,82 @@
+import requests, json
+import urllib
+
+import DataFormatter
+
+
+
+class OLSData:
+    def __init__(self, term_iri):
+        self.term_iri = term_iri
+
+    
+    def get_ols_term(self):
+        '''
+        Use OLS Term API to get details about an ontology term. 
+        '''
+
+        term_iri = self.term_iri
+        term_iri_double_encoded = urllib.quote_plus(urllib.quote_plus(term_iri))
+
+
+        # TODO: Make robust to the term/ontology being removed from OLS
+        OLS_URL = "http://www.ebi.ac.uk/ols/api/ontologies/{ontology_prefix:s}/terms/"\
+            "{term_iri:s}".format(ontology_prefix='efo',term_iri=term_iri_double_encoded)
+
+        no_results = {'iri': None, 'synonyms': None, 'short_form': None, 'label': None, 'description': None}
+
+        try:
+            response = requests.get(OLS_URL)
+            if response.status_code == 200:
+                results = json.loads(response.content)
+
+                if results:
+                    data_formatter = DataFormatter.DataFormatter(results)
+                    return (data_formatter.get_term_information())
+
+                else:
+                    print "** No data returned!!!"
+                    return no_results
+            
+            else:
+                # TODO: Handle case when EFO term is not yet in production EFO and OLS
+                # and OLS 500 return error
+                # print "\n--> ReTry OLS...", term_iri_double_encoded, "\n"
+                # get_ols_term(term_iri_double_encoded)
+                return no_results
+        
+        except requests.exceptions.RequestException as e:
+            print e
+
+
+    def get_ancestors(self):
+        '''
+        Use OLS to get ancestors for a term. NOTE: This link is from the "term"
+        web service and is already URL double-encoded.
+        '''
+
+        OLS_ANCESTOR_URL = self.term_iri
+
+        no_ancestor_results = []
+
+        try:
+            response = requests.get(OLS_ANCESTOR_URL)
+            if response.status_code == 200:
+                results = json.loads(response.content)
+
+                if results:
+                    data_formatter = DataFormatter.DataFormatter(results)
+                    return (data_formatter.get_ancestor_labels())
+
+                else:
+                    # print "** No data returned!!!"
+                    return no_ancestor_results
+            
+            else:
+                return no_results
+        
+        except requests.exceptions.RequestException as e:
+            print e
+
+
+
