87a513aadc99f39416780ec4b26db12b4820b4b3
DSuveges <suvi.dani@gmail.com>
2018-09-14 15:54:17 +0100

    Other checks were implemented in the document checking stage to make script more robuts. Also variant id changed to make it consistent.

--- scripts/document_types/variant.py
+++ scripts/document_types/variant.py
@@ -32,7 +32,7 @@ def get_variant_data(connection, limit=0):
         # Extracting merged rsID:
         current_rsID = variant_cls.get_current_rsID(ID)
 
-        # Assign merged RsID and generate title:
+        # Assign merged rsID and generate title:
         title = ''
         if current_rsID:
             merged_rsID = rsID
@@ -45,7 +45,7 @@ def get_variant_data(connection, limit=0):
         # Combining data into a dictionary:
         varDoc = {
             'resourcename' : resourcename,
-            'id' : "%s-%s" % (resourcename,ID),
+            'id' : "%s:%s" % (resourcename,ID),
             'title' : title,
             'rsID' : rsID,
             'current_rsID' : current_rsID,
